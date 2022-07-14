1464b8e0b2a20a188ec915956edbfb4c2fbb3fbf
hegdeshashi <46744305+hegdeshashi@users.noreply.github.com>
2022-07-14 15:24:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Edit <code>src/App.js</code> and save to reload.Welcome to Hexaware world..
+           Edit <code>src/App.js</code> and save to reload.Hi Shashi, Welcome to Hexaware world..
         </p>
         <a
           className="App-link"
